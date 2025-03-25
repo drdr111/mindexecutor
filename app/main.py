c5ee94b5ae83556c639af9fd8dd20b06a0605d87
drdr111 <35653227+drdr111@users.noreply.github.com>
2025-03-26 00:31:54 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,4 +6,7 @@ app = FastAPI()
 @app.get("/trello.yaml", response_class=FileResponse)
 async def get_openapi_yaml():
     return FileResponse("trello.yaml", media_type="application/yaml")
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
