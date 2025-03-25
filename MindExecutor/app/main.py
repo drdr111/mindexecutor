f98d0cba94db75e3c6a7d4bf4cfee803374e78a7
drdr111 <35653227+drdr111@users.noreply.github.com>
2025-03-25 17:32:16 +0500

    Delete MindExecutor/app directory

--- MindExecutor/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
-app = FastAPI()
-
-@app.get("/trello.yaml", response_class=FileResponse)
-async def get_openapi_yaml():
-    return FileResponse("trello.yaml", media_type="text/yaml")
